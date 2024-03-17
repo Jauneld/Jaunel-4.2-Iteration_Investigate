2c500097d647d95a97e10638ca5947f31d1457f9
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-17 04:04:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+number = 7# declares and initilizes a variable called number and assigned the value 7
+print("I'm thinking of a number, can you guess it?")#output a message to use to prompt an input 
+guess = int(input())#declares and initilizes a variable called guess and assigns it the value of the input
+while guess != number:# if the number is not equal to the guess it will trigger the loop
+  print("Wrong! Guess again!") # output the message that the user guessed wrong 
+  guess = input() #The user input will be assigned to the guess variable
+print("You guessed it!")#Outside the loop it will output the message you guessed it
+
+# Give the line number where iteration starts.
+  # Answer: The iteration starts in Line 4 where the while statment is created. 
+
+# What does '!=' mean?
+  # Answer: The operator "!=" means not equal to in python. 
+
+# How many variables are there in the code?
+  # Answer: There are 2 variables in the code. The variables are number and guess. Number's value is constant and guess's value is changing.
+
+# How can you tell which lines of code are inside the loop?
+  # Answer: You can tell that Lines 5 and 6 are in the code because they are indented which shows that something is in the loop.
+
+# How many times will the loop repeat?
+  # Answer: The loop will repeat as long as the value in the variable guess is not the same as the value in the variable number. 
+
+# What has to happen to make the program run the last line of code?
+  # Answer: The while loop has to be proven false so it ends and the last line of code will run. 
+
+#Jaunel Deans 
+#November 2, 2023
+#We used comments to explain what the code does and answer investigative questions
